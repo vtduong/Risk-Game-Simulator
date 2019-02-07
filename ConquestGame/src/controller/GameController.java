5d38d6661b56021b29329dd18a55c10f9d14e0dd
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2019-02-06 20:52:15 -0500

    Skeleton class

--- ConquestGame/src/controller/GameController.java
+++ ConquestGame/src/controller/GameController.java
@@ -1,5 +1,53 @@
 package controller;
 
-public class GameController {
+import java.util.HashMap;
+import java.util.Scanner;
 
+import beans.*;
+import phases.TurnPhase;
+
+/*
+ * @description :
+ * @author
+ */
+public class GameController {
+	
+	HashMap<Player,WorldMap> countryOwnership = new HashMap<Player,WorldMap>();
+	int numberOfPlayers;
+	
+	/*
+	 * @description :
+	 * @author
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    //TODO
+		
+	}
+	
+	/*
+	 * @description :
+	 * @author
+	 */
+	public boolean loadMap(){
+		//TODO
+		return false;
+		
+	}
+	
+	/*
+	 * @description :
+	 * @author
+	 */
+	public boolean setPhase(TurnPhase turnPhase) {
+		return false;
+	}
+    
+	/*
+	 * @description :
+	 * @author
+	 */
+	public boolean nextPhase() {
+		return false;
+	}
 }
