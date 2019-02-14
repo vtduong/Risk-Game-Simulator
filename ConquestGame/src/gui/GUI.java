7e3114830330c8b37cfc90eb9cfdb4901cbfe435
Van Duong <vanduong1610@gmail.com>
2019-02-14 00:10:58 -0500

    add fortification, testing needed

--- ConquestGame/src/gui/GUI.java
+++ ConquestGame/src/gui/GUI.java
@@ -3,12 +3,30 @@
  */
 package gui;
 
+import utilities.Tuple;
+
 /**
  * The user interface of the application. 
  * @author vanduong
  *
  */
 public class GUI {
+
+	/**
+	 * @return a tuple containing: country to move armies from, country to move armies to, and number of armies
+	 */
+	public static Tuple getFortificationInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @return true of user is done with fortification, false otherwise
+	 */
+	public static boolean isDoneFortification() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 
 }
