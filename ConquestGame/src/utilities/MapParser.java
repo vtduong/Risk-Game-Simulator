0c54e1cc0ff9cf00d98e73cec8eb390a028da18d
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2019-02-21 17:19:39 -0500

    map parser

---
+++ ConquestGame/src/utilities/MapParser.java
@@ -0,0 +1,35 @@
+package utilities;
+import java.io.File;
+import java.util.List;
+
+import beans.Continent;
+import beans.Country;;
+
+/** 
+ * 
+ * @author apoorvasharma
+ *
+ */
+
+public class MapParser {
+	File mapFile;
+	MapParser(File mapFile){
+		this.mapFile = mapFile;
+	}
+	
+	private void parseMap() {
+		
+	}
+	
+	private List<Continent> parseContinents(){
+		List<Continent> continentList = null;
+		return continentList = null;
+	}
+	
+	private List<Country>  parseCountries() {
+		List<Country> countryList = null;
+		return countryList;
+	}
+
+
+}
