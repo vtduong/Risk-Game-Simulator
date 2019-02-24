503f93568789f10cbf9a680d276a2e24da765783
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2019-02-23 19:55:24 -0500

    Parser Charges : Added function for ajd countries

--- ConquestGame/src/utilities/MapParser.java
+++ ConquestGame/src/utilities/MapParser.java
@@ -1,7 +1,6 @@
 package utilities;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import beans.Continent;
@@ -44,10 +43,11 @@ public class MapParser {
 			}
 					    
 			continentsList=parseContinents(buildMapFile.substring(buildMapFile.indexOf("[Continents]"), buildMapFile.indexOf("[Territories]")));
-			countriesList = new ArrayList<Country>();
 			countriesList=parseCountries(buildMapFile.substring(buildMapFile.indexOf("[Territories]")));
 		}catch(Exception e) {
-			
+			System.out.println("Exception :"+ e.getStackTrace());
+			System.out.println("Exception :"+ e.getMessage());
+			System.out.println("Exception :"+ e.getClass());
 		}
 		
 	}
@@ -63,7 +63,7 @@ public class MapParser {
 		String continent[] = continents.split("\n");
 		try {
 			for(int i=1;i<continent.length;i++){
-				if(!continent[i].equalsIgnoreCase("[Continents]")) {
+				if(!continent[i].equalsIgnoreCase("[Continents]") && continent[i]!=null) {
 					String[] temp = continent[i].trim().split("=");
 					Continent con = new Continent();
 					con.setControlValue(temp[1].trim());
@@ -74,6 +74,8 @@ public class MapParser {
 		}catch(Exception e) {
 			//TODO
 			System.out.println("Exception :"+ e.getStackTrace());
+			System.out.println("Exception :"+ e.getMessage());
+			System.out.println("Exception :"+ e.getClass());
 		}
 		return continentList;
 	}
@@ -87,18 +89,63 @@ public class MapParser {
 		ArrayList<Country> countryList =new ArrayList<Country>();
 		System.out.println("Parsing Countries!!");
 		String country[] = countries.split("\n");
-		for(int i =1;i<country.length;i++) {
-			String[] temp = country[i].trim().split(","); //Alaska,70,126,North America,
-			Country con = new Country();
-			con.setName(temp[0].trim());
-			con.setLatitude(Integer.parseInt(temp[1].trim()));
-			con.setlongitude(Integer.parseInt(temp[2].trim()));
-			con.setContinent(temp[3].trim());
-			countryList.add(con);
-			//System.out.println(country[1].trim()+" "+" temp[0].trim()"+temp[3].trim());
+		System.out.println(country.length);
+		try {
+		 for(int i =1;i<country.length;i++) {
+			if(!country[i].isEmpty() && !country[i].equalsIgnoreCase("[Territories]")) {
+				String[] temp = country[i].trim().split(","); //Alaska,70,126,North America,
+				if(temp!=null) {
+					Country con = new Country();
+					con.setName(temp[0].trim());
+					con.setLatitude(Integer.parseInt(temp[1].trim()));
+					con.setlongitude(Integer.parseInt(temp[2].trim()));
+					con.setContinent(temp[3].trim());
+					countryList.add(con);
+			
+				}
+			}
 		}
-		System.out.println(countryList.size());
+		addAdjacentCountries(country,countryList);
+		Country recToReturn = new Country();
+		recToReturn =getCountry("Alaska",countryList);
+		System.out.println(recToReturn.getAdjacentCountries());
+		
+		}catch(Exception e) {
+			System.out.println("Exception :"+ e.getStackTrace());
+			System.out.println("Exception :"+ e.getMessage());
+			System.out.println("Exception :"+ e.getClass());
+		}
 		return countryList;
+	
+	}
+	private static Country getCountry(String name, ArrayList<Country> countryList) {
+		Country recToReturn = new Country();
+		for(Country rec :countryList) {
+				if(rec.getName().equals(name)) {
+					recToReturn= rec;
+				}
+		}
+		
+		return recToReturn;
+	}
+	
+	/** 
+	 * 
+	 * @param countries
+	 * @return
+	 */
+	private static void addAdjacentCountries(String country[],ArrayList<Country> countryList) {
+		for(int i =1;i<country.length;i++) {
+			String[] temp = country[i].trim().split(",");
+			Country rec = getCountry(temp[0].trim(), countryList);
+			ArrayList<String> adjCountry =new ArrayList<String>();
+			for(int j=4;j<temp.length;j++) {
+				if(temp[j]!=null) {
+					adjCountry.add(temp[j].trim());
+				}
+			}
+			rec.setAdjacentCountries(adjCountry);
+		}
 	}
 
 
