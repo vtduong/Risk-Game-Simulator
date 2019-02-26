902e02a730caee635dfe2a8df866276d4f047f43
Van Duong <vanduong1610@gmail.com>
2019-02-26 14:28:45 -0500

    Fortification with adjacency check

--- ConquestGame/src/phases/Fortification.java
+++ ConquestGame/src/phases/Fortification.java
@@ -1,5 +1,7 @@
 package phases;
 
+import java.util.List;
+
 import beans.Country;
 import beans.Player;
 import controller.GameController;
@@ -40,8 +42,15 @@ public class Fortification implements TurnPhase{
 	private void moveArmies(String fromCountry, String toCountry, int numArmies) throws IllegalArgumentException {
 		Country from = curPlayer.getCountryByName(fromCountry);
 		Country to = curPlayer.getCountryByName(toCountry);
+		List<String> adjFrom = from.getAdjacentCountries();
+		List<String> adjTo = to.getAdjacentCountries();
+		//check if 2 countries are adjacent
+		if(adjFrom == null || adjTo == null || !adjFrom.contains(to.getName()) || !adjTo.contains(from.getName())) {
+			throw new IllegalArgumentException("Two countries must be adjacent");
+		}
+		//check if number of armies move is valid
 		if(from.getNumArmies() - numArmies < 1) {
-			throw new IllegalArgumentException("there must be at least 1 army in one country");
+			throw new IllegalArgumentException("there must be at least 1 army per country");
 		}
 		from.setNumArmies(from.getNumArmies() - numArmies);
 		to.setNumArmies(to.getNumArmies() + numArmies);
