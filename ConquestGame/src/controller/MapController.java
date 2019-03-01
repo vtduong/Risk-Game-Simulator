9ceb820f28867c1ff675497790d224a8d98b6b62
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2019-02-28 21:52:10 -0500

    Add Country and Continent :Edit Map Related Changes

--- ConquestGame/src/controller/MapController.java
+++ ConquestGame/src/controller/MapController.java
@@ -4,66 +4,39 @@ import beans.Continent;
 import beans.Country;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MapController {
 
-	public static void main(String argv[]) throws IOException {
-		String filepath = "src/resources/results.map";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(filepath, false));
-		Scanner scan = new Scanner(System.in);
-		setMapDetails(bw);
-		Map<String, Integer> continentMap = new HashMap<String, Integer>();
-		System.out.println("Please Enter Map Details");
-		System.out.println(
-				"Please input the numbers of continet you wish to add." + "The value should be grater than 1.");
-		int continentNumber = scan.nextInt();
-		scan.nextLine();
-		String tempname, controlValue;
-		for (int i = 0; i < continentNumber; i++) {
-
-			System.out.println("Enter Continent Name");
-			tempname = scan.nextLine();
-			System.out.println("Enter Continent value");
-			controlValue = scan.nextLine();
-			continentMap.put(tempname, Integer.parseInt(controlValue));
-		}
-		// Call add continent Method
-		addContinent(continentMap, bw);
-
-		// adding Countries
-		System.out.println(
-				"Please input the numbers of countries you wish to add." + "The value should be grater than 1.");
-
-		int countryNo = scan.nextInt();
-		scan.nextLine();
-		String tempStr;
-		String [] countryList = new String[countryNo];
-		System.out.println("Enter Country Details in Order specified!!");
-		for (int i = 0; i < countryNo; i++) {
-			tempStr = scan.nextLine();
-			countryList[i] =(tempStr);
-		}
-		addCountry(countryList,bw);
-		
-		bw.close();
-		scan.close();
-		validateMap(filepath);
-	}
-
-	private void createMap() throws IOException {
-	}
 
 	private static void validateMap(String inputFile) throws IOException {
 		new utilities.MapValidator(inputFile).createCountryGraph();
 	}
 
-	private void editMap(String inputFile) {
+	private void editMap(String inputFile, String operation) {
+
+		if ("Add Continent".equalsIgnoreCase(operation)) {
+
+		} else if ("Remove Continent".equalsIgnoreCase(operation)) {
+
+		} else if ("Add Country".equalsIgnoreCase(operation)) {
+
+		} else if ("Remove Country".equalsIgnoreCase(operation)) {
+
+		} else if ("Add Adjacent Country".equalsIgnoreCase(operation)) {
+
+		} else if ("Remove Adjacent Country".equalsIgnoreCase(operation)) {
+
+		}
 
 	}
 
@@ -71,28 +44,75 @@ public class MapController {
 		bw.write("[Map]");
 	}
 
-	private static void addContinent(Map<String, Integer> continentMap, BufferedWriter bw) throws IOException {
-		bw.write("[Continents]" + "\n");
-		for (String key : continentMap.keySet()) {
-			bw.write(key + "=" + continentMap.get(key) + "\n");
+	private static void addContinent(Map<String, Integer> continentMap, BufferedWriter bw, boolean isEdit,
+			String inputFile) throws IOException {
+
+		if (!isEdit) {
+			bw.write("[Continents]" + "\n");
+			for (String key : continentMap.keySet()) {
+				bw.write(key + "=" + continentMap.get(key) + "\n");
+			}
+		} else {
+			FileReader editFile = new FileReader(inputFile);
+			Scanner sc = new Scanner(new File(inputFile));
+			String tempStr = null;
+			String buildMapFile = "";
+			while (sc.hasNext()) {
+				tempStr = sc.nextLine();
+				buildMapFile = buildMapFile + "" + tempStr + "\n";
+			}
+			String temp = null, updatedStr = null;
+			for (String key : continentMap.keySet()) {
+				temp = (key + "=" + continentMap.get(key) + "\n");
+			}
+            //System.out.println(buildMapFile.substring(0,buildMapFile.indexOf("[Continents]")));
+			updatedStr = buildMapFile.substring(0, buildMapFile.indexOf("[Territories]")) + temp
+					+ buildMapFile.substring(buildMapFile.indexOf("[Territories]"));
+			FileWriter writer = new FileWriter(inputFile);
+			
+			writer.write(updatedStr);
+			writer.close();
 		}
 
 	}
 
-	private static void addCountry(String [] arrayList,BufferedWriter bw) throws IOException {
-		bw.write("[Territories]" + "\n");
-		for(int k=0;k<arrayList.length;k++) {
-			bw.write(arrayList[k] +"\n");
+	private static void addCountry(String[] arrayList, BufferedWriter bw, boolean isEdit, String inputFile)
+			throws IOException {
+		if(!isEdit) {
+			bw.write("[Territories]" + "\n");
+			for (int k = 0; k < arrayList.length; k++) {
+				bw.write(arrayList[k] + "\n");
+			}
+		}else {
+			FileReader editFile = new FileReader(inputFile);
+			Scanner sc = new Scanner(new File(inputFile));
+			String tempStr = null;
+			String buildMapFile = "";
+			while (sc.hasNext()) {
+				tempStr = sc.nextLine();
+				buildMapFile = buildMapFile + "" + tempStr + "\n";
+			}
+			String temp = null, updatedStr = null;
+			for (int k = 0; k < arrayList.length; k++) {
+				temp = arrayList[k] + "\n";
+			}
+            System.out.println(buildMapFile.substring(buildMapFile.indexOf("[Territories]")));
+            System.out.println(temp);
+            updatedStr = buildMapFile.substring(0, buildMapFile.indexOf("[Territories]"))
+					+ buildMapFile.substring(buildMapFile.indexOf("[Territories]"))+temp;
+            System.out.println(updatedStr);
+			FileWriter writer = new FileWriter(inputFile);
+			writer.write(updatedStr);
+			writer.close();
 		}
-		
 
 	}
 
-	private void removeContinet() {
+	private void removeContinent(String[] continentName,String inputFile) {
 
 	}
 
-	private void removeCountry() {
+	private void removeCountry(String[] continentName,String inputFile) {
 
 	}
 
