814dd667a749c9717d94676974d7e889b8d003e5
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2019-03-05 17:08:11 -0500

    Validation and testing bug fix

--- ConquestGame/src/utilities/EditMap.java
+++ ConquestGame/src/utilities/EditMap.java
@@ -1,4 +1,5 @@
 package utilities;
+
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -15,8 +16,10 @@ import beans.Continent;
 import beans.Country;
 import controller.MapController;
 import exception.MapInvalidException;
+
 /**
  * This class is used to Edit an existing Map by taking user input
+ * 
  * @author ankit
  *
  */
@@ -26,43 +29,42 @@ public class EditMap {
 	private ArrayList<String> removeContinents = null;
 	private ArrayList<String> removeCountries = null;
 	private ArrayList<String> removeAdjacentCountries = null;
-	private Map<String,List<String>> adjMap =null;
-	public List<Country> countryDefault =null;
-	
-	
-	private Map<String,List<String>> adjCountryMap = null;
+	private Map<String, List<String>> adjMap = null;
+	public List<Country> countryDefault = null;
+
+	private Map<String, List<String>> adjCountryMap = null;
 	private static EditMap editMap = null;
 	private String EDITEDMAP = "src/resources/usermap.map";
 	private String EXISTINGMAP = "src/resources/World.map";
 	private MapController mapController;
-	
-	
+
 	private EditMap() {
-		adjCountryMap = new HashMap<String,List<String>>();
+		adjCountryMap = new HashMap<String, List<String>>();
 		removeContinents = new ArrayList<String>();
 		removeAdjacentCountries = new ArrayList<String>();
 		removeCountries = new ArrayList<String>();
 		continents = new HashMap<String, Integer>();
 		countries = new ArrayList<String>();
-		adjMap     = new HashMap<String,List<String>>();
+		adjMap = new HashMap<String, List<String>>();
 		countryDefault = new ArrayList<Country>();
 	}
 
 	/**
-	 * This is used to create a singleton object of 
-	 * EditMap class.
+	 * This is used to create a singleton object of EditMap class.
+	 * 
 	 * @return EditMap
 	 */
 	public static EditMap getInstance() {
-		if(editMap == null) {
+		if (editMap == null) {
 			editMap = new EditMap();
 		}
 		return editMap;
 	}
-	
+
 	/**
-	 * This method is used to clone the existing map and then edit the new map 
-	 * by taking user input 
+	 * This method is used to clone the existing map and then edit the new map by
+	 * taking user input
+	 * 
 	 * @throws IOException
 	 * @throws MapInvalidException
 	 */
@@ -74,15 +76,15 @@ public class EditMap {
 		countryDefault = mapParser.countriesList;
 		mapController = new MapController();
 		System.out.println("----------Continent----------");
-		for(Continent continent : mapParser.continentsList)
+		for (Continent continent : mapParser.continentsList)
 			System.out.println(continent.getName());
-		
+
 		System.out.println("----------Country----------");
-		for(Country country : mapParser.countriesList)
+		for (Country country : mapParser.countriesList)
 			System.out.println(country.getName());
-		
-		while(true) {
-			
+
+		while (true) {
+
 			System.out.println("----------1)Add Continent----------");
 			System.out.println("----------2)Remove Continent----------");
 			System.out.println("----------3)Add Country----------");
@@ -90,51 +92,49 @@ public class EditMap {
 			System.out.println("----------5)Add Adjacency----------");
 			System.out.println("----------6)Remove Adjacency----------");
 			System.out.println("----------7)Exit----------");
-			
+
 			Scanner getUserOption = new Scanner(System.in);
 			int option = getUserOption.nextInt();
 			Scanner getUserInputStr = new Scanner(System.in);
 			Scanner getUserInputInt = new Scanner(System.in);
-			
-			if(option < 1 && option > 5) {
+
+			if (option < 1 && option > 5) {
 				System.out.println("Invalid option!!!!");
 				System.exit(0);
 			}
-			
-			
-				
-			if(option == 1) {
+
+			if (option == 1) {
 				System.out.print("Enter the Continent Name: ");
 				String cotinentName = getUserInputStr.nextLine();
 				System.out.println();
 				System.out.println("Enter the control value: ");
 				System.out.println();
 				int controlValue = getUserInputInt.nextInt();
-				continents.put(cotinentName, controlValue);	
+				continents.put(cotinentName, controlValue);
 			}
-			
-			else if(option == 2) {
+
+			else if (option == 2) {
 				System.out.print("Enter the Continent Name: ");
 				String cotinentName = getUserInputStr.nextLine();
 				System.out.println();
 				removeContinents.add(cotinentName);
 			}
-			
-			else if(option == 3) {
+
+			else if (option == 3) {
 				System.out.print("Enter the Country Name: ");
 				String countryName = getUserInputStr.nextLine();
 				System.out.println();
 				countries.add(countryName);
 			}
-			
-			else if(option == 4) {
+
+			else if (option == 4) {
 				System.out.print("Enter the Country Name: ");
 				String countryName = getUserInputStr.nextLine();
 				System.out.println();
 				removeCountries.add(countryName);
 			}
-			
-			else if(option == 5) {
+
+			else if (option == 5) {
 				ArrayList<String> addAdjacentCountries = new ArrayList<String>();
 				System.out.println("Enter the Country Name: ");
 				String countryName = getUserInputStr.nextLine();
@@ -142,70 +142,73 @@ public class EditMap {
 				int adjLen = 0;
 				System.out.println("Enter number of adjacent list: ");
 				adjLen = getUserInputInt.nextInt();
-				
-				for(int i =0; i < adjLen; i++) {
+
+				for (int i = 0; i < adjLen; i++) {
 					addAdjacentCountries.add(getUserInputStr.nextLine());
 					System.out.println();
 				}
-					
+
 				adjCountryMap.put(countryName, addAdjacentCountries);
 			}
-			
-			else if(option == 6) {
-				adjMap = new HashMap<String,List<String>>();
+
+			else if (option == 6) {
+				adjMap = new HashMap<String, List<String>>();
 				System.out.println("Enter the Country Name: ");
 				String countryName = getUserInputStr.nextLine();
 				System.out.println();
 				System.out.println("Enter the number of adjacent Countries to be removed: ");
-				int conNo =getUserInputInt.nextInt();
+				int conNo = getUserInputInt.nextInt();
 				System.out.println("Enter the name of adjacent Countries to be removed: ");
-				for(int i=0;i<conNo;i++) {
+				for (int i = 0; i < conNo; i++) {
 					String adjCountryName = getUserInputStr.nextLine();
-					if(adjMap.containsKey(countryName)){
+					if (adjMap.containsKey(countryName)) {
 						adjMap.get(countryName).add(adjCountryName);
-					}else {
-						List<String> tempList =new ArrayList<String>();
+					} else {
+						List<String> tempList = new ArrayList<String>();
 						tempList.add(adjCountryName);
-						adjMap.put(countryName,tempList);
+						adjMap.put(countryName, tempList);
 					}
-					if(adjMap.containsKey(adjCountryName)) {
+					if (adjMap.containsKey(adjCountryName)) {
 						adjMap.get(adjCountryName).add(countryName);
-					}else {
-						List<String> tempList =new ArrayList<String>();
+					} else {
+						List<String> tempList = new ArrayList<String>();
 						tempList.add(countryName);
-						adjMap.put(adjCountryName,tempList);
+						adjMap.put(adjCountryName, tempList);
 					}
 				}
-				
+
 			}
-			
-			else 
+
+			else
 				break;
-			
-			
-			}
-		
-			
-		if(removeCountries.size() > 0)
-			mapController.removeCountry(removeCountries, EDITEDMAP,false);
-		
-		if(removeContinents.size() > 0)
+
+		}
+
+		if (removeCountries.size() > 0) {
+			mapController.removeCountry(removeCountries, EDITEDMAP, false);
+		}
+
+		if (removeContinents.size() > 0) {
 			mapController.removeContinent(removeContinents, EDITEDMAP);
-		
-		if(continents.size() > 0)
-			mapController.addContinent(continents,null, true, EDITEDMAP);
-		
-		if(countries.size() > 0)
+		}
+
+		if (continents.size() > 0) {
+			mapController.addContinent(continents, null, true, EDITEDMAP);
+		}
+		if (countries.size() > 0) {
 			mapController.addCountry(countries.toArray(), null, true, EDITEDMAP);
-		
-		if(adjMap.size() > 0)
+		}
+
+		if (adjMap.size() > 0) {
 			System.out.print(adjMap.size());
-			mapController.removeAdjCountry(null, EDITEDMAP,adjMap,false);
-		
-		if(adjCountryMap.size() > 0)
+			mapController.removeAdjCountry(null, EDITEDMAP, adjMap, false);
+		}
+
+		if (adjCountryMap.size() > 0) {
 			mapController.addAdjCountry(adjCountryMap, EDITEDMAP);
-		    
+		}
+
 		mapController.validateMap(EDITEDMAP);
 	}
-		
+
 }
