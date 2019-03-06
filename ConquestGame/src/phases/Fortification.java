67a48426ee417f4d5887b55c27de0b20dfa1ba11
Van Duong <vanduong1610@gmail.com>
2019-03-06 16:50:22 -0500

    add more input validation for phases, add more error messages

--- ConquestGame/src/phases/Fortification.java
+++ ConquestGame/src/phases/Fortification.java
@@ -33,7 +33,13 @@ public class Fortification implements TurnPhase{
 		//move armies from one (and only one) country to another neighboring country
 		String fromName = controller.selectCountryToTransferFrom(curPlayer.getPlayerCountries());
 		Country fromCountry = curPlayer.getCountryByName(fromName);
+		if(fromCountry == null) {
+			throw new IllegalArgumentException(fromName + " is not a country you occupied!");
+		}
 		List<String> adjCountries = fromCountry.getAdjacentCountries();
+		if(adjCountries == null || adjCountries.size() == 0) {
+			throw new IllegalArgumentException("There is no adjacent countries occupied by you!");
+		}
 		//get the names of countries occupied by this player among adjacent countries
 		List<String> occupiedCountries = new ArrayList<String>();
 		for(int i = 0; i < adjCountries.size(); i++) {
@@ -50,18 +56,21 @@ public class Fortification implements TurnPhase{
 		moveArmies(fromName, toName, numArmies);
 	}
 
-//	/**
-//	 * Move armies.
-//	 *
-//	 * @param fromCountry the country from which armies are transfered
-//	 * @param toCountry   the country to which armies are transfered
-//	 * @param numArmies   number of armies transfered
-//	 * @exception IllegalArgumentException There must be at least one army in one
-//	 *                                     country
-//	 */
+	/**
+	 * Move armies.
+	 *
+	 * @param fromCountry the country from which armies are transfered
+	 * @param toCountry   the country to which armies are transfered
+	 * @param numArmies   number of armies transfered
+	 * @exception IllegalArgumentException There must be at least one army in one
+	 *                                     country
+	 */
 	private void moveArmies(String fromCountry, String toCountry, int numArmies) throws IllegalArgumentException {
 		Country from = curPlayer.getCountryByName(fromCountry);
 		Country to = curPlayer.getCountryByName(toCountry);
+		if( from == null || to == null) {
+			throw new IllegalArgumentException("The selected country is not occupied by you!");
+		}
 		List<String> adjFrom = from.getAdjacentCountries();
 		List<String> adjTo = to.getAdjacentCountries();
 		//check if 2 countries are adjacent
@@ -71,6 +80,9 @@ public class Fortification implements TurnPhase{
 		//check if number of armies move is valid
 		if(from.getNumArmies() - numArmies < 1) {
 			throw new IllegalArgumentException("there must be at least 1 army per country");
+		}
+		if(numArmies < 0) {
+			throw new IllegalArgumentException("Number of armies must be non-negative!");
 		}
 		from.setNumArmies(from.getNumArmies() - numArmies);
 		to.setNumArmies(to.getNumArmies() + numArmies);
