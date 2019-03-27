143091a3dcf1910cab63e7779771c73423d79dae
ankitjana <ankit92005@gmail.com>
2019-03-26 20:29:12 -0400

    phase view javadoc

--- ConquestGame/src/gui/PhaseView.java
+++ ConquestGame/src/gui/PhaseView.java
@@ -4,12 +4,10 @@ import controller.GameController;
 
 // TODO: Auto-generated Javadoc
 /**
- * Implementation of a �phase view� using the Observer pattern. The phase view should display:
+ * Implementation of a phase view using the Observer pattern. The phase view should display:
  * (1)The name of the game phase currently being played 
- * (2)The current player�s name
- * (3)Information about actions that are taking place during this phase. 
+ * (2)The current player name
  * The phase view should be cleared at the beginning of every phase.
- * @author ankit
  *
  */
 public class PhaseView implements Observer {
